397c30556362547d87d04a362551d8c6c27e2453
cmunhozprog <cmunhozprog03@hotmail.com>
2020-09-28 18:42:38 -0300

    EP 10 Complete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,11 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
+                                    @can('manage_users')
+                                    <a href="{{route('admin.users.index')}}">
+                                        User management
+                                    </a>
+                                    @endcan
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
